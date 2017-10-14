9365a7637932909e28ee94d125f4aa8a1f55cef9
Nick Allen <blingusblongus@users.noreply.github.com>
2017-10-14 11:43:56 -0500

    working, no final syllable support yet

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 //sets
-const bcdfSet = ['ring', 'ling', 'rong', 'long', 'roing', 'rang', 'ram', 'rom', 'roim', 'om', 'oim', 'im'],
+// Currently, comp is technically compSet + nonCompSet
+/*
+comp: bcfgkp //comp+semiComp+nonComp
+semiComp: dtw //semiComp+nonComp
+nonComp: hjlmnrsvxyz //nonComp
+other: q //own thing
+*/
+const compSet = ['ling', 'long'],
+      semiCompSet = ['ring', 'rong', 'roing', 'rang', 'ram', 'rom', 'roim'],
+      nonCompSet = ['om', 'oim', 'im', 'am', 'ing', 'oing', 'ong', 'ang'],
       second_global = ['bop', 'blip', 'bip', 'dong'],
-      second_ing = ['us'],
+      second_ing = ['us'], //weight this one heavier
       second_not_ing = ['bus'];
 
+function control(name){
+    //app exec code here
+    //get syllables
+    const syl = syllables(name);
+    let result = [];
+
+    //for each syllable, add a piece (arrow function?)
+    syl.map(el => {
+        console.log(el);
+        let set = chooseSet(el);
+        let rand = Math.floor(Math.random()*set.length);
+
+        result.push(el);
+        result.push(set[rand]);
+    });
+
+    return result.join('');
+}
+
 function syllables(str){
-    //returns array of
+    //returns array of syllables without linking consonants
 
     const vowels = /[aeiou]/g;
     let cur, last = 0, match, result=[];
@@ -23,10 +51,19 @@ function syllables(str){
 function chooseSet(substr){
     const last = substr[substr.length - 1];
 
-    if(/bcdf/.match(last)){
-        //addl. code goes here (mergeArrays)
-    }else if{
-        //blah blah blah
+    // Regex
+    const compRe = /[bcfgkp]/,
+          semiCompRe = /[dtw]/,
+          nonCompRe = /[hjlmnrsvxyz]/;
+
+    if(compRe.exec(last)){
+        return mergeArrays([compSet, semiCompSet, nonCompSet]);
+    }else if(semiCompRe.exec(last)){
+        return mergeArrays([semiCompSet, nonCompSet]);
+    }else if(nonCompRe.exec(last)){
+        return nonCompSet;
+    }else if(last=='q'){
+        //Special set - add u, then nonCompSet
     }
 }
 
